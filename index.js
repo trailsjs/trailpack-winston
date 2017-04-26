cfbc73a726b2ab26f75fbfe58986e77781a7eb6a
Travis J. Webb <me@traviswebb.com>
2017-04-26 03:00:03 -0400

    [update] compatibility with trails v3 log events

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ const winston = require('winston')
 
 module.exports = class WinstonTrailpack extends Trailpack {
 
-  async initialize () {
-    this.app.log = new winston.Logger(this.app.config.log)
+  bindEvents () {
+    this.app.removeAllListeners('trails:log')
+    this.app.on('trails:log', (level, msg = [ ]) => this.logger.log(level, ...msg))
   }
 
+  /**
+   * Logging is a system function, and we thus initialize the logger in the
+   * constructor phase, before the Trails app has started.
+   */
   constructor (app) {
     super(app, {
       config: require('./config'),
       api: require('./api'),
       pkg: require('./package')
     })
+
+    this.logger = new winston.Logger(this.app.config.log)
+    this.bindEvents()
   }
 }
 
